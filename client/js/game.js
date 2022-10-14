504378e31ad1503039f75d23ef645075e8106c27
Pierre <R2Jeu.prive@gmail.com>
2022-10-14 16:31:51 +0200

    added grid

--- client/js/game.js
+++ client/js/game.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
 });
 
-function test(){
-    console.log("test");
+grid = [];
+
+function initGrid(){
+    $(".game-screen .grid").html("");
+    let sectorNames = ["a","b","c","d","e","f","g","h","i"];
+    for (let i = 0; i < 9; i++) {
+        $(".game-screen .grid").append('<div class="' + sectorNames[i] + '">');
+    }
+
+    for (let i = 0; i < 9; i++) {
+        grid.push([]);
+        for (let j = 0; j < 9; j++) {
+            grid[i].push(0);
+            updateGrid(i, j, Math.randomInt(0,3));
+        }
+    }
+}
+
+function updateGrid(x, y, val){
+    grid[i][j] = val;
+    //animate;
 }
