30dd7859a7bc12e8c414fb761110021934844a78
Pierre <R2Jeu.prive@gmail.com>
2022-10-14 18:50:50 +0200

    fixed bug with order

--- client/js/game.js
+++ client/js/game.js
@@ -28,11 +28,11 @@ function initGrid(){
     for (let x = 0; x < 9; x++) {
         grid.push([]);
         for (let y = 0; y < 9; y++) {
-            grid[x].push(0);
-            updateGrid(x, y, Math.floor(Math.random()*3));
-
             let sector = sectorNames[3*Math.floor(x/3) + Math.floor(y/3)];
             $("#game-screen ." + sector).append('<div class="' + x + "" + y + ' cells"></div>');
+            
+            grid[x].push(0);
+            updateGrid(x, y, Math.floor(Math.random()*3));
         }
     }
 
