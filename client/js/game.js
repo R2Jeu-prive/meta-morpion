b52391be5ed13f52424ce0d21cceefda9f47b67c
Pierre <R2Jeu.prive@gmail.com>
2022-10-14 16:55:28 +0200

    info and added classes

--- client/js/game.js
+++ client/js/game.js
@@ -4,10 +4,16 @@ $(document).ready(function() {
 grid = [];
 
 function initGrid(){
+    /*    x->
+      y  A  D  G
+      |  B  E  H
+      V  C  F  I
+    */
+
     $("#game-screen .grid").html("");
-    let sectorNames = ["a","b","c","d","e","f","g","h","i"];//in column order
+    let sectorNames = ["a","b","c","d","e","f","g","h","i"];
     for (let i = 0; i < 9; i++) {
-        $("#game-screen .grid").append('<div class="' + sectorNames[i] + '">');
+        $("#game-screen .grid").append('<div class="' + sectorNames[i] + ' sectors">');
     }
 
     for (let x = 0; x < 9; x++) {
@@ -17,7 +23,7 @@ function initGrid(){
             updateGrid(x, y, Math.floor(Math.random()*3));
 
             let sector = sectorNames[3*Math.floor(x/3) + Math.floor(y/3)];
-            $("#game-screen ." + sector).append('<div class="' + x + "" + y + '">');
+            $("#game-screen ." + sector).append('<div class="' + x + "" + y + ' cells">');
         }
     }
 }
