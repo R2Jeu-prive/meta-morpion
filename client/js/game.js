4d3f360acb4cdf4f776e70ee39138626662dc74d
Pierre <R2Jeu.prive@gmail.com>
2022-10-16 19:50:38 +0200

    fixed grid layout

--- client/js/game.js
+++ client/js/game.js
@@ -35,14 +35,13 @@ function initGrid(){
     }
 
     //cells
-    for (let x = 0; x < 9; x++) {
-        grid.push([]);
-        for (let y = 0; y < 9; y++) {
-            let sector = sectorNames[3*Math.floor(y/3) + Math.floor(x/3)];
+    for (let y = 0; y < 9; y++) {
+        grid.push([0,0,0,0,0,0,0,0,0]);
+        for (let x = 0; x < 9; x++) {
+            let sector = sectorNames[3*Math.floor(x/3) + Math.floor(y/3)];
             $("#game-screen ." + sector).append('<div class="' + x + "" + y + ' cells"></div>');
             $("#game-screen ." + x + "" + y).append('<svg class="circle" width="100%" height="100%"><circle cx="50%" cy="50%" r="30%" stroke="blue" stroke-width="10%" fill="#00000000" pathLength="100" stroke-linecap="round" stroke-dasharray="100" stroke-dashoffset="100"></circle></svg>');
             $("#game-screen ." + x + "" + y).append('<svg class="cross" width="100%" height="100%"><line stroke="orange" stroke-width="10%" pathLength="100" stroke-linecap="round" stroke-dasharray="100" stroke-dashoffset="100" x1="25%" y1="25%" x2="75%" y2="75%" class="later"></line><line stroke="orange" stroke-width="10%" pathLength="100" stroke-linecap="round" stroke-dasharray="100" stroke-dashoffset="100" x1="25%" y1="75%" x2="75%" y2="25%"></line></svg>');
-            grid[x].push(0);
         }
     }
 
